efa39f005ec4b92fcc58b915a1c598eaaad09e70
Kittenlover229 <artur.roos.artur@gmail.com>
2023-10-11 21:07:43 +0300

    Fix the board

--- src/common/board.c
+++ src/common/board.c
@@ -1,72 +1,20 @@
 #include "./common/board.h"
 
+#include <memory.h>
+
 #define GROW_BOARD_CAPACITY(c) ((c + 1) * 3)
-
-bool
-a_lt_cmp_b(i4 a_x, i4 a_y, i4 b_x, i4 b_y) {
-  if (a_x < b_x)
-    if (a_y < b_y)
-      return true;
-  return false;
-}
-
-sz
-board_get_impl(board_cell* data, sz len, i4 x, i4 y) {
-  if (len == 0)
-    return len;
-
-  sz l = 0;
-  sz r = len - 1;
-  while (l <= r) {
-    sz m = (l + r) / 2;
-    board_cell c = data[m];
-    if (a_lt_cmp_b(c.x, c.y, x, y))
-      l = m + 1;
-    else if (a_lt_cmp_b(x, y, c.x, c.y))
-      r = m - 1;
-    else
-      return m;
-  }
-
-  return len;
-}
 
 board_mark*
 board_get(board* self, i4 x, i4 y) {
-  sz idx = board_get_impl(self->data, self->size, x, y);
-  return idx == self->size ? NULL : &self->data[idx].mark;
-}
-
-void
-board_insort_impl(board_cell* data, sz len, i4 x, i4 y, board_mark mark) {
-  if (len != 0) {
-    sz l = 0;
-    sz r = len - 1;
-    while (l <= r) {
-      sz m = (l + r) / 2;
-      board_cell c = data[m];
-      if (a_lt_cmp_b(c.x, c.y, x, y))
-        l = m + 1;
-      else if (a_lt_cmp_b(x, y, c.x, c.y))
-        r = m - 1;
-      else {
-        // TODO: maybe deny overwrite?
-        data[m].mark = mark;
-        return;
-      }
-    }
-    data[(l + r) / 2].x = x;
-    data[(l + r) / 2].y = y;
-    data[(l + r) / 2].mark = mark;
-  } else {
-    data[0].x = x;
-    data[0].y = y;
-    data[0].mark = mark;
-  }
+  for (sz i = 0; i < self->size; i++)
+    if (self->data[i].x == x && self->data[i].y == y)
+      return &self->data[i].mark;
+  return NULL;
 }
 
 void
 board_insort(board* self, i4 x, i4 y, board_mark mark) {
+  // TODO: actual sorted insert
   if (self->capacity < self->size + 1) {
     sz new_capacity = GROW_BOARD_CAPACITY(self->capacity);
     board_cell* data =
@@ -76,6 +24,12 @@ board_insort(board* self, i4 x, i4 y, board_mark mark) {
     self->capacity = new_capacity;
   }
 
-  board_insort_impl(self->data, self->size, x, y, mark);
-  self->size++;
+  board_mark* mark_ptr = board_get(self, x, y);
+  if (mark_ptr != NULL) {
+    *mark_ptr = mark;
+  } else {
+    self->data[self->size].x = x;
+    self->data[self->size].y = y;
+    self->data[self->size++].mark = mark;
+  }
 }
--- src/tests/test_board.c
+++ src/tests/test_board.c
@@ -1,6 +1,7 @@
 #include "./common/board.h"
 #include "./common/types.h"
 
+#include <stdio.h>
 #include <assert.h>
 
 i4
@@ -10,13 +11,19 @@ main() {
   board b = board_new();
   assert(board_get(&b, 0, 0) == NULL);
 
-  board_insort(&b, 0, 0, BOARD_MARK_CROSS);
-  assert(*board_get(&b, 0, 0) == BOARD_MARK_CROSS);
+  board_insort(&b, 0, 0, BOARD_MARK_KNOT);
+  assert(*board_get(&b, 0, 0) == BOARD_MARK_KNOT);
 
-  board_insort(&b, 0, 0, BOARD_MARK_CROSS);
-  board_insort(&b, 0, 2, BOARD_MARK_CROSS);
-  board_insort(&b, 4, 0, BOARD_MARK_CROSS);
-  board_insort(&b, 1, 1, BOARD_MARK_CROSS);
-  board_insort(&b, 7, 8, BOARD_MARK_CROSS);
-  board_insort(&b, 2, 9, BOARD_MARK_CROSS);
+  board_insort(&b, 0, 0, (board_mark)0xF1);
+  board_insort(&b, 0, 2, (board_mark)0xF2);
+  board_insort(&b, 4, 0, (board_mark)0xF3);
+  board_insort(&b, 1, 1, (board_mark)0xF4);
+  board_insort(&b, 7, 8, (board_mark)0xF5);
+  board_insort(&b, 2, 9, (board_mark)0xF6);
+
+  assert(b.size == 6);
+  for (sz i = 0; i < 6; i++)
+    assert(b.data[i].mark == 0xF1 + i);
+
+  return 0;
 }
